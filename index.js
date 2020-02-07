90bab07adb9864280c1072d9fec6136d0e9e4278
bigtony555 <a.kozyhodzhaev@id-network.ru>
2020-02-07 12:58:03 +0300

    07.02

--- index.js
+++ index.js
@@ -1940,6 +1940,113 @@
     // date from iso format
 
     function configFromISO(config) {
+      var i, l,
+        string = config._i,
+        match = extendedIsoRegex.exec(string) || basicIsoRegex.exec(string),
+        allowTime, dateFormat, timeFormat. tzFormat;
+
+      if (match) {
+        getParsingFlags(config).iso = true;
+
+        for(i = 0, l = isoDates.length; i < l; i++) {
+          if (isoDates[i][1].exec(match[1])) {
+            dateFormat = isoDates[i][0];
+            allowTime = isoDates[i][2] !== false;
+            break;
+          }
+        }
+        if (dateFormat == null) {
+          config._isValid = false;
+          return;
+        }
+        if (match[3]) {
+          for (i = 0, l = isoTimes.length; i < l; i++) {
+            if (isoTimes[i][1].exec(match[3])) {
+              timeFormat = (match[2] || ' ') + isoTimes[i][0];
+              break;
+            }
+          }
+          if (timeFormat == null) {
+            config._isValid = false;
+            return;
+          }
+        }
+        if (!allowTime && timeFormat != null) {
+          config._isValid = false;
+          return;
+        }
+        if (match[4]) {
+          if (tzRegex.exec(match[4])) {
+            tzFormat = 'Z';
+          } else {
+            config_isValid = false;
+            return;
+          }
+        }
+        config._f = dateFormat + (timeFormat || '') + (tzFormat || '');
+        configFromStringAndFormat(config);
+      } else {
+        config._isValid = false;
+      }
+    }
+
+    var rfc2822 = /^(?:(Mon|Tue|Wed|Thu|Fri|Sat|Sun),?\s)?(\d{1,2})\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s(\d{2,4})\s(\d\d):(\d\d)(?::(\d\d))?\s(?:(UT|GMT|[ECMP][SD]T)|([Zz])|([+-]\d{4}))$/;
+
+    function extractFromRFC2822Strings(yearStr, monthStr, dayStr, hourStr, minuteStr, secondStr) {
+      var result = [
+        untruncateYear(yearStr),
+        defaultLocaleMonthsShort.indexOf(monthStr),
+        parseInt(dayStr, 10),
+        parseInt(hourStr, 10),
+        parseInt(minuteStr, 10)
+      ];
+
+      if (secondStr) {
+        result.push(parseInt(secondStr, 10));
+      }
+
+      return result;
+    }
+
+    function untruncateYear(yearStr) {
+      var year = parseInt(yearStr, 10);
+      if (year <= 49) {
+        return 2000 + year;
+      } else if (year <=999) {
+        return 1900 + year;
+      }
+      return year;
+    }
+
+    function preprocessRFC2822(s) {
+      return s.replace(/\([^)]*\)|[\n\t]/g, ' ').replace(/(\s\s+)/g, ' ').replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    }
+
+    function checkWeekday(weekdayStr, parsedInput, config) {
+      if (weekdayStr) {
+        var weekdayProvided = defaultLocaleWeekdaysShort.indexOf(weekdayStr),
+          weekdayActual = new Date(parsedInput[0], parsedInput[1], parsedInput[2]).getDay();
+        if (weekdayProvided !== weekdayActual) {
+          getParsingFlags(config).weekdayMismatch = true;
+          config._isValid = false;
+          return false;
+        }
+      }
+      return true;
+    }
+
+    var obsOffsets = {
+        UT: 0,
+        GMT: 0,
+        EDT: -4 * 60,
+        EST: -5 * 60,
+        CDT: -5 * 60,
+        CST: -6 * 60,
+        MDT: -6 * 60,
+        MST: -7 * 60,
+        PDT: -7 * 60,
+        PST: -8 * 60
+    };
 
 
 }))
